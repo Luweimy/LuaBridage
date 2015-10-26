05120f3270da52671f0cd32aeef7781ebb379b8a
luwei <luweimy@126.com>
2015-10-26 18:02:00 +0800

    modify copyright

--- LuaBridage.cpp
+++ LuaBridage.cpp
@@ -1,9 +1,8 @@
 //
 //  Lua.h
-//  musegame
 //
 //  Created by Luwei on 15/5/19.
-//  Copyright (c) 2015 MUSE-GAME. All rights reserved.
+//  Copyright (c) 2015 Luwei. All rights reserved.
 //
 
 #include "LuaBridage.h"
